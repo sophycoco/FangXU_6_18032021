968aaf038f9d749e43261bc50eb58f98e85c6f15
xf <sophycoco@gmail.com>
2021-08-11 03:22:46 +0200

    Update images

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,4 +1,5 @@
 const Thing = require('../models/thing');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
     const thingObject = JSON.parse(req.body.sauce);
@@ -13,15 +14,27 @@ exports.createThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
+    const thingObject = req.file ?
+      { 
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      } : { ...req.body };
     Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(()=> res.status(200).json({ message: 'modifié '}))
     .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteThing = (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
+    Thing.findOne({ _id: req.params.id })
+    .then(thing => {
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink(`${filename}`, () => {
+        Thing.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 exports.getOneThing = (req, res, next) => {
