e663e95b49589c68c1ad002368f0f9d3dc398142
xf <sophycoco@gmail.com>
2021-08-24 12:16:38 +0200

    Add password control

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router(); 
 const userCtrl = require('../controllers/user');
+const control = require('../middleware/control-password');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', control, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
